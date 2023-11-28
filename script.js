2789727550e91d22ef9cd090269791d96170019f
jabrams513 <145389973+jabrams513@users.noreply.github.com>
2023-11-28 12:26:30 -0500

    add some preliminary comments. create a placeholder variable quizBank where sample questions, responses, and answers are stored

---
+++ script.js
@@ -0,0 +1,34 @@
+// Assign variables to DOM elements and attributes
+
+// Create a bank for questions, responses, and correct answers
+let quizBank = [
+    {
+        question: "Question 1: What is question 1?",
+        responses: ["A","B","C","D"],
+        correctAnswer: "A",
+    },
+    {
+        question: "Question 2: What is question 2?",
+        responses: ["A","B","C","D"],
+        correctAnswer: "B",
+    },
+    {
+        question: "Question 3: What is question 3?",
+        responses: ["A","B","C","D"],
+        correctAnswer: "C",
+    },
+    {
+        question: "Question 4: What is question 4?",
+        responses: ["A","B","C","D"],
+        correctAnswer: "D",
+    },
+    {
+        question: "Question 5: What is question 5?",
+        responses: ["A","B","C","D"],
+        correctAnswer: "A",
+    },
+]
+
+// Timer starts when you click on Start Quiz
+
+// Question pops up when you click start quiz
